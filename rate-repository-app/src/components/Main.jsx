0bab6595efc723b18c3ee5e946c252471595f91e
des-ant <57375593+des-ant@users.noreply.github.com>
2022-01-24 22:37:08 +1100

    Implement routing using react-router-native

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,5 +1,7 @@
 import Constants from 'expo-constants';
 import { StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
+
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 import theme from '../theme';
@@ -17,7 +19,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
