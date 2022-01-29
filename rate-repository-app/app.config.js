8813f51a307ca8dc9df44847f42897bd9e054b53
des-ant <57375593+des-ant@users.noreply.github.com>
2022-01-29 22:52:52 +1100

    Use an environment variable defined in the .env file when initializing the Apollo Client. -ex10.12

--- rate-repository-app/app.config.js
+++ rate-repository-app/app.config.js
@@ -31,5 +31,6 @@ export default {
   },
   extra: {
     env: process.env.ENV,
+    apolloUri: process.env.APOLLO_URI,
   },
 };
--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
+
+const { apolloUri } = Constants.manifest.extra;
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.0.26:4000/graphql',
+  uri: apolloUri,
 });
 
 const createApolloClient = () => {
