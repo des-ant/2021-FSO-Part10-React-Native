8f42fc7c631ed539c166669fbc656779c5c03c04
des-ant <57375593+des-ant@users.noreply.github.com>
2022-01-30 17:16:15 +1100

    Integrate the storage to the useSignIn hook. Use React Context for dependency injection.

--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,9 +1,11 @@
 import { useMutation } from '@apollo/client';
 
 import { SIGN_IN } from '../graphql/mutations';
+import useAuthStorage from './useAuthStorage';
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(SIGN_IN);
+  const authStorage = useAuthStorage();
 
   const signIn = async ({ username, password }) => {
     return await mutate({ variables: {
