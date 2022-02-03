8a440615a93d804a93b1d74e377b2470b93cc38c
des-ant <57375593+des-ant@users.noreply.github.com>
2022-02-03 19:40:10 +1100

    Add Repository details fragment and add query for fetching single repository from Apollo server.

---
+++ rate-repository-app/src/graphql/fragments.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const REPOSITORY_DETAILS = gql`
+  fragment RepositoryDetails on Repository {
+    id
+    fullName
+    stargazersCount
+    ownerAvatarUrl
+    description
+    language
+    forksCount
+    reviewCount
+    ratingAverage
+  }
+`;
